7ae1847776e51d946ad9efa26bcc68ed240d07f5
Wei-ju Wu <weiju.wu@gmail.com>
2012-09-28 15:16:37 -0700

    updated start script

--- genomebrowser.sh
+++ genomebrowser.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp lib/gaggle.jar:lib/sqlitejdbc-v056.jar:lib/log4j-1.2.14.jar:dist/genomebrowser.jar org.systemsbiology.genomebrowser.Main
+java -cp lib/colorpicker.jar:lib/glazedlists-1.8.0_java15.jar:ui.jar:lib/commons-lang3-3.1.jar:lib/gaggle.jar:lib/sqlitejdbc-v056.jar:lib/log4j-1.2.14.jar:dist/genomebrowser.jar org.systemsbiology.genomebrowser.Main
