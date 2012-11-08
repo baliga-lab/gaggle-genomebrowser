2fd731373a827acd38f42272142485da427021a3
Wei-ju Wu <weiju.wu@gmail.com>
2012-11-08 10:35:19 -0800

    ported ProgressListenerSupport.java

--- genomebrowser-core/src/main/scala/util/Events.scala
+++ genomebrowser-core/src/main/scala/util/Events.scala
@@ -106,3 +106,117 @@ extends ProgressReporter with Runnable {
 
   def start { new Thread(this).start }
 }
+
+/**
+ * Support code for a class that provides progress events.
+ */
+class ProgressListenerSupport
+extends Iterable[ProgressListener] with ProgressReporter with ProgressListener {
+
+  private val listeners = new CopyOnWriteArraySet[ProgressListener]
+
+  def addProgressListener(l: ProgressListener) = listeners.add(l)
+  def removeProgressListener(l: ProgressListener) = listeners.remove(l)
+  def iterator = listeners.iterator
+  def fireIncrementProgressEvent(amount: Int) = listeners.map(_.incrementProgress(amount))
+  def fireProgressEvent(progress: Int) = listeners.map(_.setProgress(progress))
+  def fireProgressEvent(progress: Int, expected: Int) {
+    listeners.map(listener => {
+      listener.setExpectedProgress(expected);
+      listener.setProgress(progress);
+    })
+  }
+  def fireSetExpectedProgressEvent(expected: Int) =
+    listeners.map(_.setExpectedProgress(expected))
+  
+  def fireInitEvent(expected: Int) = listeners.map(_.init(expected))
+  def fireInitEvent(expected: Int, message: String) = listeners.map(_.init(expected, message))
+  def fireMessageEvent(message: String) = listeners.map(_.setMessage(message))
+  def fireDoneEvent = listeners.map(_.done)
+
+  // ---- progress listener methods -----------------------------------------
+	
+  // We implement these for forwarding progress from one source to listeners
+  // of this source. This usually means that a process that reports progress
+  // is being used as a part of a larger process. So, we it makes sense to
+  // forward increments and messages, but probably not other progress events
+
+  def done { }
+  def init(expected: Int) { }
+  def init(expected: Int, message: String) { }
+  def setExpectedProgress(expected: Int) { }
+  def setProgress(progress: Int) { }
+  def incrementProgress(amount: Int) = fireIncrementProgressEvent(amount)
+  def setMessage(message: String) = fireMessageEvent(message)
+}
+
+/*
+class ProgressListenerWrapper extends ProgressListener {
+  private ProgressListenerSupport progressListenerSupport;
+    private int start;
+    private int width;
+    private int expected = 100;
+
+    public ProgressListenerWrapper() {
+        progressListenerSupport = new ProgressListenerSupport();
+    }
+
+    public ProgressListenerWrapper(ProgressListenerSupport progressListenerSupport) {
+        this.progressListenerSupport = progressListenerSupport;
+    }
+
+    /**
+     * Scale the progress reporting to fit within a subinterval of
+     * the progress of a larger process. Let's say we have a 5 step
+     * process. Step 2 reports its own progress on a scale of 0-100.
+     * We want to adapt the progress of step 2 to cover the range of
+     * 20-20
+     * @param start
+     * @param end
+     */
+    public void scaleProgressToFit(int start, int end) {
+        this.start = start;
+        this.width = end - start;
+    }
+
+    public void done() {
+        progressListenerSupport.fireDoneEvent();
+    }
+
+    public void incrementProgress(int amount) {
+        progressListenerSupport.fireIncrementProgressEvent(amount);
+    }
+
+    public void init(int expected) {
+        this.expected = expected;
+        progressListenerSupport.fireInitEvent(expected);
+    }
+    public void init(int expected, String message) {
+        this.expected = expected;
+        progressListenerSupport.fireInitEvent(expected, message);
+    }
+
+    public void setExpectedProgress(int expected) {
+        this.expected = expected;
+        if (width <= 0)	progressListenerSupport.fireSetExpectedProgressEvent(expected);
+    }
+
+    public void setMessage(String message) {
+        progressListenerSupport.fireMessageEvent(message);
+    }
+
+    public void setProgress(int progress) {
+        int scaledProgress = width > 0 ?
+            (int)(start + ((double)progress) / ((double)expected) * width) :  progress;
+        progressListenerSupport.fireProgressEvent(scaledProgress);
+    }
+
+    public void addProgressListener(ProgressListener listener) {
+        progressListenerSupport.addProgressListener(listener);
+    }
+
+    public void removeProgressListener(ProgressListener listener) {
+        progressListenerSupport.removeProgressListener(listener);
+    }
+}
+*/
