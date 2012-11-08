8ad9be60a98040ec94d3f58ff3774d0c23438889
Wei-ju Wu <weiju.wu@gmail.com>
2012-11-08 11:39:46 -0800

    ported BasicFeature and FeatureSource

--- genomebrowser-core/src/main/scala/model/Feature.scala
+++ genomebrowser-core/src/main/scala/model/Feature.scala
@@ -1,7 +1,9 @@
 package org.systemsbiology.genomebrowser.model
 
+import scala.reflect.BeanProperty
 import java.util.{List, Iterator}
-import org.systemsbiology.util.Iteratable
+import org.systemsbiology.util.{Iteratable, ProgressListener}
+import org.systemsbiology.util.MathUtils
 
 // TODO should it be FeatureFields responsibility to validate Strand and GeneType values?
 // Those values must come from a restricted set of strings.
@@ -46,6 +48,39 @@ trait FeatureProcessor {
 }
 
 /**
+ * The purpose of this interface is to connect a source of features to
+ * a "sink" for features -- the FeatureProcessor. It is used, for example,
+ * to load features from various file formats and store them in a datastore,
+ * whose implementation is supposed to be flexible. The source can stream
+ * feature information to limit memory usage.
+ * 
+ * The pattern used here is a double-dispatch something like the Visitor
+ * pattern in order to process features in a manner dependent on both source
+ * and consumer.
+ * 
+ * I considered a producer-consumer implementation using a thread-safe queue,
+ * but the concurrency complicates things without much benefit. The real issue
+ * here is to allow variation of both the producer and consumer and not to
+ * manage the workload in any special way.
+ * 
+ * A FeatureSource is like an iterator except the FeatureSource pushes features
+ * to the FeatureProcessor rather than having the consumer pull features from
+ * the iterator. This makes it easier for the FeatureSource to ensure that
+ * proper cleanup happens, such as closing files.
+ *
+ * It's still possible that there's one too many levels of indirection going on
+ * here or maybe an Iterator would have worked just as well. 
+ */
+trait FeatureSource {
+  @throws(classOf[Exception])
+  def processFeatures(featureProcessor: FeatureProcessor)
+
+  // TODO remove progress methods from FeatureSource
+  def addProgressListener(progressListener: ProgressListener)
+  def removeProgressListener(progressListener: ProgressListener)
+}
+
+/**
  * Iterates the subset of a list of features that falls inside the range
  * given by the start and end constructor parameters.
  */
@@ -71,4 +106,19 @@ extends Iteratable[F] {
 		throw new UnsupportedOperationException("remove not supported in feature iterators");
 
 	def iterator : Iterator[F] = this
+}
+
+class BasicFeature(@BeanProperty var seqId: String,
+                   @BeanProperty var strand: Strand,
+                   @BeanProperty var start: Int,
+                   @BeanProperty var end: Int,
+                   var label: String) extends Feature {
+  def this(seqId: String, strand: Strand, start: Int, end: Int) =
+    this(seqId, strand, start, end, null)
+
+  def getLabel = if (label == null)
+    "%s%s:%d-%d".format(seqId, strand.toAbbreviatedString, start, end)
+                 else label
+  def setLabel(label: String) { this.label = label }
+  def getCentralPosition: Int = MathUtils.average(start, end)
 }
